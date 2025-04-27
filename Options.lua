dbb2b64c2bd93c13b3fdd8d422919942600b3d59
OrionShock <orionshock@gmail.com>
2025-04-27 16:02:33 -0700

    don't use UIDropDownMenu system

--- Options.lua
+++ Options.lua
@@ -10,45 +10,111 @@ frame:SetScript("OnShow", function(self)
     title:SetPoint("TOPLEFT", 10, -10)
     title:SetText("TransmogMailer Options")
 
-    -- Modifier key dropdown
+    -- Modifier key button and menu
     local modifierLabel = self:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     modifierLabel:SetPoint("TOPLEFT", 10, -40)
     modifierLabel:SetText("Modifier Key:")
 
-    local modifierDropdown = CreateFrame("Frame", addonName .. "ModifierDropdown", self, "UIDropDownMenuTemplate")
-    modifierDropdown:SetPoint("TOPLEFT", 120, -35)
-    UIDropDownMenu_SetWidth(modifierDropdown, 100)
-    UIDropDownMenu_SetText(modifierDropdown, TransmogMailerDB.modifier or "SHIFT")
+    local modifierButton = CreateFrame("Button", addonName .. "ModifierButton", self, "UIPanelButtonTemplate")
+    modifierButton:SetPoint("TOPLEFT", 120, -35)
+    modifierButton:SetSize(100, 22)
+    modifierButton:SetText(TransmogMailerDB.modifier or "SHIFT")
 
-    UIDropDownMenu_Initialize(modifierDropdown, function(self)
-        local info = UIDropDownMenu_CreateInfo()
-        for _, key in ipairs({ "SHIFT", "CTRL", "ALT" }) do
-            info.text = key
-            info.func = function()
-                TransmogMailerDB.modifier = key
-                UIDropDownMenu_SetText(modifierDropdown, key)
-            end
-            UIDropDownMenu_AddButton(info)
-        end
+    local modifierMenu = CreateFrame("Frame", addonName .. "ModifierMenu", self, "MenuFrameTemplate")
+    modifierMenu:SetPoint("TOPLEFT", modifierButton, "BOTTOMLEFT", 0, 0)
+    modifierMenu:Hide()
+
+    local function UpdateModifierButton(modifier)
+        TransmogMailerDB.modifier = modifier
+        modifierButton:SetText(modifier)
+    end
+
+    -- Define menu options
+    local modifierOptions = {
+        { text = "SHIFT", func = function() UpdateModifierButton("SHIFT") end },
+        { text = "CTRL", func = function() UpdateModifierButton("CTRL") end },
+        { text = "ALT", func = function() UpdateModifierButton("ALT") end },
+    }
+
+    -- Populate the menu
+    for _, option in ipairs(modifierOptions) do
+        Menu.ModifyMenu("MENU_" .. addonName .. "ModifierMenu", function(owner, rootDescription)
+            rootDescription:CreateButton(option.text, option.func)
+        end)
+    end
+
+    modifierButton:SetScript("OnClick", function()
+        Menu.OpenMenu(modifierMenu, modifierButton)
     end)
 
     -- Armor class mappings
-    local armorTypes = { "Cloth", "Leather", "Mail", "Plate" }
+    local armorTypes = {
+        {key = "Cloth", label = "Cloth"},
+        {key = "Leather", label = "Leather"},
+        {key = "Mail", label = "Mail"},
+        {key = "Plate", label = "Plate"}
+    }
+
+    -- Weapon type mappings
+    local weaponTypes = {
+        {key = "OneHandAxe", label = "One-Handed Axe"},
+        {key = "TwoHandAxe", label = "Two-Handed Axe"},
+        {key = "OneHandMace", label = "One-Handed Mace"},
+        {key = "TwoHandMace", label = "Two-Handed Mace"},
+        {key = "OneHandSword", label = "One-Handed Sword"},
+        {key = "TwoHandSword", label = "Two-Handed Sword"},
+        {key = "Staff", label = "Staff"},
+        {key = "Polearm", label = "Polearm"},
+        {key = "Bow", label = "Bow"},
+        {key = "Crossbow", label = "Crossbow"},
+        {key = "Gun", label = "Gun"},
+        {key = "Dagger", label = "Dagger"},
+        {key = "Fist", label = "Fist Weapon"},
+        {key = "Wand", label = "Wand"}
+    }
+
     local yOffset = -80
-    for i, armorType in ipairs(armorTypes) do
+    -- Armor mappings
+    for _, armor in ipairs(armorTypes) do
         local label = self:CreateFontString(nil, "OVERLAY", "GameFontNormal")
         label:SetPoint("TOPLEFT", 10, yOffset)
-        label:SetText(armorType .. " Recipient:")
+        label:SetText(armor.label .. " Recipient:")
 
-        local editBox = CreateFrame("EditBox", addonName .. armorType .. "EditBox", self, "InputBoxTemplate")
+        local editBox = CreateFrame("EditBox", addonName .. armor.key .. "EditBox", self, "InputBoxTemplate")
         editBox:SetPoint("TOPLEFT", 120, yOffset)
         editBox:SetSize(150, 20)
-        editBox:SetText(TransmogMailerDB.mappings[armorType] or "")
+        editBox:SetText(TransmogMailerDB.mappings[armor.key] or "")
         editBox:SetScript("OnEnterPressed", function(self)
-            TransmogMailerDB.mappings[armorType] = self:GetText()
+            TransmogMailerDB.mappings[armor.key] = self:GetText()
         end)
         editBox:SetScript("OnEditFocusLost", function(self)
-            TransmogMailerDB.mappings[armorType] = self:GetText()
+            TransmogMailerDB.mappings[armor.key] = self:GetText()
+        end)
+
+        yOffset = yOffset - 30
+    end
+
+    -- Weapon mappings
+    yOffset = yOffset - 20 -- Add some spacing
+    local weaponTitle = self:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+    weaponTitle:SetPoint("TOPLEFT", 10, yOffset)
+    weaponTitle:SetText("Weapon Recipients")
+    yOffset = yOffset - 30
+
+    for _, weapon in ipairs(weaponTypes) do
+        local label = self:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+        label:SetPoint("TOPLEFT", 10, yOffset)
+        label:SetText(weapon.label .. " Recipient:")
+
+        local editBox = CreateFrame("EditBox", addonName .. weapon.key .. "EditBox", self, "InputBoxTemplate")
+        editBox:SetPoint("TOPLEFT", 120, yOffset)
+        editBox:SetSize(150, 20)
+        editBox:SetText(TransmogMailerDB.mappings[weapon.key] or "")
+        editBox:SetScript("OnEnterPressed", function(self)
+            TransmogMailerDB.mappings[weapon.key] = self:GetText()
+        end)
+        editBox:SetScript("OnEditFocusLost", function(self)
+            TransmogMailerDB.mappings[weapon.key] = self:GetText()
         end)
 
         yOffset = yOffset - 30
